70a8dc447f3c3c4e78f6ea4aa0aa7b50b834c088
GrantGit123 <gaguir9@illinois.edu>
2018-01-31 12:43:49 -0600

    Attempt at maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,31 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+
+            while(!maze.isFinished()){
+                if(maze.canMove()){
+                    maze.move();
+                }
+                else if(!maze.canMove()){
+                    while(!maze.canMove()) {
+                        maze.turnRight();
+                        if(!maze.canMove()) {
+                            maze.turnLeft();
+                            maze.turnLeft();
+                            maze.turnLeft();
+                            maze.move();
+                        }
+                        maze.move();
+                    }
+                }
+                if(maze.isFinished()){
+                    break;
+                }
+            }
+
+
+
+
         }
 
         if (maze.isFinished()) {
